773bc1d091a7d9858cc107efdc29d5bf08666843
Sudarshan Poudel <poudelsudarshan650@gmail.com>
2024-12-22 17:24:30 +0545

    fixed mistakes in previous commits

--- Backend/initialize_data.py
+++ Backend/initialize_data.py
@@ -1,17 +1,12 @@
-# Run this file once to initialize csv data
+# Script to add initial places
+
+from data import store_place
 import pandas as pd
-from data import detailed_desc, store_chunks
 
-# Read csv
-places_df = pd.read_csv("data/places.csv")
-places_df['description_llm'] = places_df.apply(
-    lambda row: detailed_desc(row['id'],row['name'], row['description'],  row['location'], row['longitude'], row['latitude'], row['type'], row['activities'], row['required_time']),
-    axis=1
-)
+df = pd.read_csv('data/places.csv')
 
-print("Storing places started...")
-for index, row in places_df.iterrows():
-    row_dict = row.to_dict()  
-    store_chunks(row_dict)
-    if((index+1)%10 == 0):
-        print(f"{index+1} Places added...")
+print("Storing data about places started...")
+for i, row in df.iterrows():
+    store_place(row.to_dict())
+    if((i+1)%10 == 0):
+        print(f"Added {i+1} Places")
